9225fc3f8df6710f20feb6b6a8ced69b8e65a1e7
Christopher Prince <chris@SpasticMuffin.biz>
2021-07-31 11:56:00 -0600

    version 0.0.2

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/SyncServerII/iOSSignIn.git", from: "0.5.0"),
         .package(url: "https://github.com/SyncServerII/ServerShared.git", from: "0.12.10"),
         .package(url: "https://github.com/SyncServerII/iOSShared.git", from: "0.15.0"),
-        .package(url: "https://github.com/crspybits/SolidAuthSwift.git", .branch("main")),
+        .package(url: "https://github.com/crspybits/SolidAuthSwift.git", from: "0.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
